a0377d2774a6a342496c07bb7b815e4d5aa79c96
qz <meshipsmou@yahoo.co.jp>
2022-12-08 16:48:35 +0900

    ツイート課題・編集追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-    before_action :set_blog, only: [:show, :edit, :destroy]
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
     def index
       @posts = Post.all
     end
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
         render :new
       else
         if @post.save
-          redirect_to posts_path, notice: "投稿を作成しました！"
+          redirect_to posts_path, notice: "投稿しました！"
         else
           render :new
         end
@@ -28,9 +28,17 @@ class PostsController < ApplicationController
     def edit
     end
 
+    def update
+      if @post.update(post_params)
+          redirect_to posts_path, notice: "投稿内容を編集しました！"
+      else
+          render :edit
+      end
+    end
+
     def destroy
       @post.destroy
-      redirect_to posts_path, notice:"投稿を削除しました！"
+      redirect_to posts_path, notice:"投稿内容を削除しました！"
     end
 
     def confirm
