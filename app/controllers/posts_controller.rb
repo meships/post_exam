7d685b88857e918b3ba0c79b06e7925fc4052515
qz <meshipsmou@yahoo.co.jp>
2022-12-08 14:28:23 +0900

    ツイート課題・before_action追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+  before_action :set_blog, only: [:show, :edit]
   def index
     @posts = Post.all
   end
@@ -16,10 +16,22 @@ class PostsController < ApplicationController
       render :new
     end
 
+    def show
+    end
+
+    def edit
+    end
+
+
+
     private
 
     def post_params
       params.require(:post).permit(:content)
+    end
+
+    defset_post
+      @post = Post.find(params[:id])
     end
 end
 
