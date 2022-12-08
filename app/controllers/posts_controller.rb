e1dd48ff8776c74e6a883c5d3cec17770ac15abb
qz <meshipsmou@yahoo.co.jp>
2022-12-08 13:48:30 +0900

    ツイート課題・コントローラ仮

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "コメントを投稿しました"
+    else
+      render :new
+    end
+end
